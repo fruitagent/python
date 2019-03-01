f3b68dd7156dcba789df5f34d514e55b719a5954
apple <769747912@qq.com>
2019-03-01 19:55:48 +0800

    第一次使用github

--- pygame/alien_invasion.py
+++ pygame/alien_invasion.py
@@ -1,16 +1,24 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    bg_color = (230,230,230)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    #开始游戏的主循环
 
     while True:
+        #监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        #每次循环时都重制屏幕
+        screen.fill(ai_settings.bg_color)
+        #让就近绘制的屏幕可见
         pygame.display.flip()
 
 run_game()
